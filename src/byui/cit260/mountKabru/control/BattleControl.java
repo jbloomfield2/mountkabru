11280ec1934a50a8784cfa499d65d80a64d0a58f
jbloomfield2 <farcry.15.w00t@gmail.com>
2016-02-10 11:38:32 -0700

    add skeleton classes for control layer

---
+++ src/byui/cit260/mountKabru/control/BattleControl.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mountKabru.control;
+import java.util.Random;
+
+/**
+ *
+ * @author jacob bloomfield
+ */
+public class BattleControl {
+    
+    public int attack(int atk,int atkBonus,int def){
+        Random rng = new Random();
+        int totalAttack = 0;
+        
+        //check for vaild range
+        if (atk < 1)
+            return -1;
+        
+        if (atk > 50)
+            return 50; //50 is the highest possible damage
+        
+        if (atkBonus < 1)
+            return -1;
+        
+        if (atkBonus > 50)
+            return 50;
+        
+        if (def < 1)
+            return -1;
+        
+        if (def > 50)
+            return -1;
+            
+        //now we know data is within acceptable bounds
+        totalAttack = atk + atkBonus;
+        totalAttack -= def;
+        
+        //return a 0 if defense causes damge to be negative
+        if (totalAttack < 0)
+            return 0;
+    
+        return totalAttack;
+    }
+}
