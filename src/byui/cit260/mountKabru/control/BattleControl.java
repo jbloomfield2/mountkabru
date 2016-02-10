6aa4ba4b2fb6541b9e86f7aeff68fb9148317e19
Andrew <Andrew@Lappy486>
2016-02-10 12:05:44 -0700

    Added SPELL to the BattleControl

--- src/byui/cit260/mountKabru/control/BattleControl.java
+++ src/byui/cit260/mountKabru/control/BattleControl.java
@@ -45,4 +45,38 @@ public class BattleControl {
     
         return totalAttack;
     }
+    
+    public int spell(int mana,int manaBonus,int def){
+        Random rng = new Random();
+        int totalSpell = 0;
+        
+        //check for vaild range
+        if (mana < 1)
+            return -1;
+        
+        if (mana > 50)
+            return 50; //50 is the highest possible damage
+        
+        if (manaBonus < 1)
+            return -1;
+        
+        if (manaBonus > 50)
+            return 50;
+        
+        if (def < 1)
+            return -1;
+        
+        if (def > 50)
+            return -1;
+            
+        //now we know data is within acceptable bounds
+        totalSpell = mana + manaBonus;
+        totalSpell -= def;
+        
+        //return a 0 if defense causes damge to be negative
+        if (totalSpell < 0)
+            return 0;
+    
+        return totalSpell;
+    }
 }
