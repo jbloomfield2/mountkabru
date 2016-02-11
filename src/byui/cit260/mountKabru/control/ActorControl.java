61813c29de7947f05d00d78a70b66fb31f92a657
jbloomfield2 <farcry.15.w00t@gmail.com>
2016-02-11 12:30:32 -0700

    add comments, fix misspelled variable

--- src/byui/cit260/mountKabru/control/ActorControl.java
+++ src/byui/cit260/mountKabru/control/ActorControl.java
@@ -13,42 +13,44 @@ public class ActorControl {
     
     public int talkToActor(int level){
           Random rng = new Random();
-           int responce = 0;
+           int response;
            
+           //responses will be pulled from an array of strings, responses 0-9 will appear every other level
+           //responses 10-19 will be returned randomly for each odd level
            if (level == 2)
-               return 1;
+               return 0;
            
            if (level == 4)
-               return 2;
+               return 1;
                        
            if (level == 6)
-               return 3;
+               return 2;
                        
            if (level == 8)
-               return 4;
+               return 3;
            
            if (level == 10)
-               return 5;
+               return 4;
            
            if (level == 12)
-               return 6;
+               return 5;
            
            if (level == 14)
-               return 7;
+               return 6;
            
            if (level == 16)
-               return 8;
+               return 7;
            
            if (level == 18)
-               return 9;
+               return 8;
            
            if (level == 20)
-               return 10;
-           
-          int respond = rng.nextInt(10);
-          respond += 10;
+               return 9;
+           //return a random response from 10-19
+          response = rng.nextInt(10);
+          response += 10;
           
-          return respond;
+          return response;
           
     }
 }
