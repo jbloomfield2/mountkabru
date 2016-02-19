0ec68c4ae054b0bc089d26fd640b840b0627f035
Andrew <Andrew@Lappy486>
2016-02-19 12:09:46 -0700

    Added MainMenuView

--- src/byui/cit260/mountKabru/view/StartProgramView.java
+++ src/byui/cit260/mountKabru/view/StartProgramView.java
@@ -115,7 +115,17 @@ public class StartProgramView {
     }
 
     private void displayNextView(Player player) {
-        System.out.println("\n*** displayNextView() called ***");
+        System.out.println("\n============================================="
+                           + "\n Welcome to the game " + player.getName()
+                           + "\n We hope you have a lot of fun!"
+                           + "\n============================================="
+                            );
+        
+        // Create MainMenuView object
+        MainMenuView mainMenuView = new MainMenuView();
+                
+        // Display the main menu view
+        mainMenuView.displayMainMenuView();
     }
 
 }
