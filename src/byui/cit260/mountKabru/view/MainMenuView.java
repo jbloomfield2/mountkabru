7c84800f40de2adcfa2db7b4a002b90d62b3a03c
Andrew <Andrew@Lappy486>
2016-02-19 14:50:46 -0700

    Added to the MainMenuView function

--- src/byui/cit260/mountKabru/view/MainMenuView.java
+++ src/byui/cit260/mountKabru/view/MainMenuView.java
@@ -5,12 +5,15 @@
  */
 package byui.cit260.mountKabru.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Andrew
  */
 public class MainMenuView {
     private String menu;
+    private boolean promptMessage;
     
     public MainMenuView(){
         this.menu = "\n"
@@ -26,8 +29,84 @@ public class MainMenuView {
     
 }
     
-    void displayMainMenuView() {
-        System.out.println("\n***displayMenu() function called***");
+    public void displayMainMenuView() {
+        
+        boolean done= false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+            
+        } while (!done);
+        
+        }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); // keyboard input stream
+        String value = "";
+        
+        boolean valid = false; // set flag to invalid value entered
+        while(!valid) { // while a vaild name has not been retrieved
+            
+            //promt for the player's name
+            System.out.println(this.promptMessage);
+            
+            value = keyboard.nextLine(); // get the name from the keyboard
+            value = value.trim();
+            
+            //if the name is invalid (less than one character in length)
+            if (value.length() < 1){
+                System.out.println("\nInvalid value - the value can not be blank");
+                continue; // and repeat again
+            }
+            valid = true; //set flag to end repetition
+        }
+        
+        return value; // return the value
     }
-    
+
+    public boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice){
+            case "N": // create and start a new game
+                this.startNewGame();
+                break;
+            case "G": // display the help menu
+                this.startExistingGame();
+                break;
+            case "H": // display the help menu
+                this.displayHelpMenu();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void startExistingGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void displayHelpMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void saveGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
