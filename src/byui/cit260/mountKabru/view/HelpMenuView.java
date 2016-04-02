0a42cd4d640014abd4d0e409a442232a0a5cf0c1
jbloomfield2 <farcry.15.w00t@gmail.com>
2016-04-02 15:03:50 -0600

    Help menu now more helpful

--- src/byui/cit260/mountKabru/view/HelpMenuView.java
+++ src/byui/cit260/mountKabru/view/HelpMenuView.java
@@ -23,6 +23,9 @@ class HelpMenuView extends View {
               + "\n----------------------------------------"
               + "\nG - What is the goal of the game?"
               + "\nT - The town"
+              + "\nC - character class information"
+              + "\nM - Blacksmith"
+              + "\nP - The Pit"
               + "\nA - Adventuring"
               + "\nI - Inventory"
               + "\nQ - Go back"
@@ -36,17 +39,41 @@ class HelpMenuView extends View {
         
         switch (choice){
             case "G": // Goal of game
-                this.console.println("Goal of game info");
+                this.console.println("The goal of the game is to locate the mysterious master who has created all the monsters."
+                        + "You will be able to find him once you have grown strong enough.");
                 break;
+                
             case "T": // the town
-                this.console.println("The town info");
+                this.console.println("The town is the main hub of the world. from the town you can head out in search of adventure or "
+                        + "visit the blacksmith to upgrade your gear, train at the pit, or visit the tavern to rest and accept quests");
                 break;
+                
+            case "C":
+                this.console.println("There are 3 classes available. The mageis skilled in magic and uses the fireball and lightning abilities."
+                        + " the paladin is a skilled warrior able to use utility magic like healing and intimidating enemies. the warrior is skilled in"
+                        + " melee combat and can use slash and spiral cut.");
+                break;
+                
+            case "M":
+                this.console.println("The blacksmith is where you can upgrade your weapons and armor. The cost starts out low but "
+                        + "will gradually increase after each upgrade");
+                break;
+                
+            case "P":
+                this.console.println("The pit is where you can train and improve your character's attributes. You may train and improve 3 stats every time you level up."
+                        + "be sure to use it each level, because the amount of training you can do does not add up");
+                break;
+                
             case "A": // Adventuring
-                this.console.println("Adventuring info");
+                this.console.println("Choose to head out to a region, and search for a monster. There is a small chance you won't find any monsters."
+                        + " be careful when you battle, if your health reaches 0, you will lose all of your shillings and experience for the current level."
+                        + "Available abilities will depend on the player's class.");
                 break;
+                
             case "I"://inventory
                 this.displayInventoryList();
                 break;
+                
             default:
                 this.console.println("\n*** Invalid selection *** Try again");
                 break;
